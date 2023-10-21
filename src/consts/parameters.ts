dd8fb6516cb590ac5efd3340cc6f440f062d5fb7
jekl <102255715+jekl0x@users.noreply.github.com>
2023-10-21 16:39:29 -0400

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,11 +2,11 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0x477A310c4896bE4B5bD366C2C1e2Fe22d3Be80D7";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
-export const chainConst = "mumbai";
+export const chainConst = "goerli";
 
 // It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
 // Please ensure that you define the correct domain for your API key from the API settings page.
@@ -15,7 +15,7 @@ export const chainConst = "mumbai";
 export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
 
 // Configure the primary color for buttons and other UI elements
-export const primaryColorConst = "blue";
+export const primaryColorConst = "663333";
 
 // Choose between "light" and "dark" mode
 export const themeConst = "dark";
